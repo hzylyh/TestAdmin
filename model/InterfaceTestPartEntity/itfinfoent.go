d1a7a824f94ed1b20d026086cff518ad923ab1a3
John Holl <hzylyh@hotmail.com>
2019-11-17 18:19:19 +0800

    1.接口信息添加项目Id

--- model/InterfaceTestPartEntity/itfinfoent.go
+++ model/InterfaceTestPartEntity/itfinfoent.go
@@ -6,8 +6,9 @@ import (
 
 type InterfaceInfo struct {
 	conf.Model
-	Name string `json:"name"`
-	Url  string `json:"url"`
-	Type string `json:"type"`
-	Desc string `json:"desc"`
+	ProjectId int    `json:"projectId"`
+	Name      string `json:"name"`
+	Url       string `json:"url"`
+	Type      string `json:"type"`
+	Desc      string `json:"desc"`
 }
