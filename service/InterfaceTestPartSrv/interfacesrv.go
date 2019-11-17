a0b57c7c2999cde953b0932e176f02cf752a4c3c
John Holl <hzylyh@hotmail.com>
2019-11-17 17:10:26 +0800

    1.新增分页方法

--- service/InterfaceTestPartSrv/interfacesrv.go
+++ service/InterfaceTestPartSrv/interfacesrv.go
@@ -5,6 +5,7 @@ import (
 	"salotto/model"
 	"salotto/model/InterfaceTestPartEntity"
 	"salotto/service"
+	"salotto/utils"
 	"salotto/utils/qjson"
 )
 
@@ -23,23 +24,8 @@ func (its *itfTestService) GetInterfaceList(qj *qjson.QJson) *model.PageInfo {
 	var (
 		ret      []*InterfaceTestPartEntity.InterfaceInfo
 		pageInfo *model.PageInfo
-		pageNum  = qj.GetInt("pageNum")
-		pageSize = qj.GetInt("pageSize")
-		total    float64
 	)
 
-	if err := service.DB.Limit(pageSize).Offset((pageNum - 1) * pageSize).Order("created_at desc").Find(&ret); err != nil {
-		fmt.Println(err)
-	}
-
-	// 获取总条数
-	service.DB.Model(&ret).Count(&total)
-
-	pageInfo = &model.PageInfo{
-		PageNum:  pageNum,
-		PageSize: pageSize,
-		Total:    total,
-		List:     ret,
-	}
+	pageInfo = utils.Pagination(&ret, qj)
 	return pageInfo
 }
