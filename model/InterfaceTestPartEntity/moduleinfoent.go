807cc2422a11e0829b6b0ed2ff727696c118d3c4
John Holl <hzylyh@hotmail.com>
2019-11-25 09:21:11 +0800

    1.实体类全部加上自建id字段，使用uuid

--- model/InterfaceTestPartEntity/moduleinfoent.go
+++ model/InterfaceTestPartEntity/moduleinfoent.go
@@ -5,6 +5,7 @@ import "salotto/conf"
 type ProjectModule struct {
 	conf.Model
 	ProjectId  int    `json:"projectId"`
+	ModuleId   string `json:"moduleId"`
 	PModuleId  int    `json:"pModuleId"`
 	ModuleNum  string `json:"moduleNum"`
 	ModuleName string `json:"moduleName"`
--- model/projectinfoent.go
+++ model/projectinfoent.go
@@ -4,6 +4,7 @@ import "salotto/conf"
 
 type ProjectInfo struct {
 	conf.Model
+	ProjectId   string `json:"projectId"`
 	ProjectName string `json:"projectName"`
 	ProjectDesc string `json:"projectDesc"`
 	Creator     string `json:"creator"`
